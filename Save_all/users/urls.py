becf30c6b61c72641d47bea4319198968d32189c
Amarok00 <imka0012@gmail.com>
2023-08-27 16:44:32 +0300

    edit: urls user

--- Save_all/users/urls.py
+++ Save_all/users/urls.py
@@ -11,11 +11,7 @@ from .views import (
 
 urlpatterns = [
     path('profile/', MyProfileView.as_view(), name='profile'),
-    path('profile/edit_avatar/',
-        EditImageAjaxView.as_view(), name='edit_image_ajax'),
     path('profile/edit/', EditProfileView.as_view(), name='edit_profile'),
-    path('profile/reset_image/',
-        ResetImageView.as_view(), name='reset-image'),
     path('profile/delete/', DeleteUserView.as_view(), name='delete-user'),
     path('<str:username>/', UserProfileView.as_view(), name='user_profile'),
 ]
