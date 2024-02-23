637798713b1bb634f87533e4370844f1fac084a6
Amarok00 <imka0012@gmail.com>
2024-02-23 20:36:57 +0300

    add app friends

--- Save_all/friends/admin.py
+++ Save_all/friends/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import FriendRequest
 
-# Register your models here.
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    """FriendRequest model admin page configuration"""
+
+    list_display = ("from_profile", "to_profile")
+    list_filter = ["from_profile", "to_profile"]
+    
